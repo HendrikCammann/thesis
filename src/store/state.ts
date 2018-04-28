8640c04284e8924c9f0c93387d1a1ce07c0fbe1b
Hendrik Cammann <hendrikcammann@MacBook-Pro.fritz.box>
2018-04-28 16:35:54 +0200

    local data is now correct

--- src/store/state.ts
+++ src/store/state.ts
@@ -3,6 +3,7 @@ import {FilterModel} from '../models/Filter/FilterModel';
 import {LoadingStatus, loadingStatus} from '../models/App/AppStatus';
 import {UserModel} from '../models/User/UserModel';
 import {ClusterItem} from '../models/State/StateModel';
+const stravaData = require('../stravadatabase/object.json');
 
 export enum RunType {
   All = 'All',
@@ -23,6 +24,29 @@ export enum ClusterType {
   ByWeeks = 'ByWeeks'
 }
 
+const blackList = ['index', 'id', 'name', 'date', 'updated_at', 'created_at'];
+const killList = ['index', 'updated_at', 'created_at'];
+
+function reformatJSON(data) {
+  for (let item in data) {
+    for (let key in data[item]) {
+      if (blackList.indexOf(key) < 0) {
+        data[item][key] = JSON.parse(data[item][key]);
+      }
+    }
+  }
+
+  for (let item in data) {
+    for (let key in data[item]) {
+      if (killList.indexOf(key) >= 0) {
+        delete data[item][key];
+      }
+    }
+  }
+
+  return data;
+}
+
 export class State {
   public appLoadingStatus: LoadingStatus;
   public user: UserModel;
@@ -37,6 +61,8 @@ export class State {
   public filter: FilterModel;
   public dashboardFilter: FilterModel;
 
+  public localActivities: ActivityModel[];
+
   constructor() {
     this.user = new UserModel();
     this.appLoadingStatus = new LoadingStatus();
@@ -50,6 +76,8 @@ export class State {
     this.selectedActivityId = null;
 
     this.selectedTrainingClusters = ['Karlsruhe-2017', 'Kandel-2017'];
+
+    this.localActivities = reformatJSON(stravaData);
 
     this.filter = new FilterModel();
 

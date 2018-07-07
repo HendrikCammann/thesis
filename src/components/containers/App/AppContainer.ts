99a990c0ef30e21680332e6f578dc2f549b8957d
Hendrik Cammann <mail@hendrikcammann.de>
2018-07-07 14:19:38 +0200

    created start page

--- src/components/containers/App/AppContainer.ts
+++ src/components/containers/App/AppContainer.ts
@@ -1,17 +1,25 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
-
+import {eventBus} from '../../../main';
+import {menuEvents} from '../../../events/Menu/menu';
+import {Button} from '../../ui-elements/button';
 
 @Component({
-  template: require('./app.html')
+  template: require('./app.html'),
+  components: {
+    'button-standard': Button
+  }
 })
 export class AppContainer extends Vue {
 
-  package: string = 'vue-vuex-boillerplate';
-  repo: string = 'https://github.com/bamboo-c/vue-vuex-boilerplate';
-  mode: string = process.env.ENV;
+  public startApp() {
+    this.$router.push({
+      path: 'dashboard'
+    });
+  }
 
   mounted() {
+    eventBus.$emit(menuEvents.set_State, 'bidh.io');
   }
 
 }

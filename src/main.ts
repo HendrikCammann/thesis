576a591d7d8919ef829edbedd34dd968acbd7c12
Hendrik Cammann <mail@hendrikcammann.de>
2018-07-07 15:26:43 +0200

    last fix from popup

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ import './styles/main.scss';
 import {formatDistance, formatPace} from './utils/format-data';
 import {FormatDistanceType, FormatPaceType} from './models/FormatModel';
 import {NavigationModule} from './components/ui-widgets/navigation';
+import {Dimension} from './components/ui-widgets/app-dimension';
 
 sync(store, router);
 
@@ -35,8 +36,24 @@ new Vue({
   router: router,
   components: {
     'navigationModule': NavigationModule,
+    'appDimension': Dimension,
+  },
+  data: {
+    show: false
+  },
+  methods: {
+    checkWidth() {
+      if (window.matchMedia('(orientation: landscape)').matches) {
+        // you're in LANDSCAPE mode
+        this.show = document.documentElement.clientWidth > 1050;
+      } else {
+        this.show = document.documentElement.clientWidth > 768;
+      }
+    }
   },
   mounted: function () {
+    this.checkWidth();
+    window.addEventListener('resize', this.checkWidth);
     if ('serviceWorker' in navigator) {
       const registration = runtime.register();
     }
